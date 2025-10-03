c5afe722dcede3a203439f3192a0140d22957b33
Jason Lee <jason.m.lee.2003@gmail.com>
2025-10-02 21:36:04 -0400

    Changed how my happy face has a different smile line but needs adjustments

--- Art-Jam/js/script.js
+++ Art-Jam/js/script.js
@@ -197,9 +197,9 @@ function drawMouth() {
             // Happy mouth
             arc(0, 45, 50, 30, 0, PI);
             // Add smile lines
-            //strokeWeight(1);
-           // line(-10, 35, -30, 30);
-           // line(10, 35, 30, 30);
+            strokeWeight(1);
+            arc(-30, 30, 20, 25, radians(20), radians(100));   // Left curve
+            arc(30, 30, 20, 25, radians(80), radians(100));    // Right curve
             } else {
             // Neutral mouth
             line(-10, 50, 15, 50);
