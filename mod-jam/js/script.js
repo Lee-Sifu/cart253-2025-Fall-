cff3a64257e6bc148c86b2d9c2bd44249f6ecb57
Jason Lee <jason.m.lee.2003@gmail.com>
2025-10-22 23:38:00 -0400

    Frog movement works. Removed the top and bottom arrow key movement

--- mod-jam/js/script.js
+++ mod-jam/js/script.js
@@ -54,12 +54,6 @@ function moveFrog() {
     if (keyIsDown(RIGHT_ARROW)) {
         frog.x += 5;
     }
-    if (keyIsDown(UP_ARROW)) {
-        frog.y -= 5;
-    }
-    if (keyIsDown(DOWN_ARROW)) {
-        frog.y += 5;
-    }
 }
 
 function drawFrog() {
