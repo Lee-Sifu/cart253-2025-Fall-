5096f78b6691ab17d3bf2cfef73a3883d072cd7a
Jason Lee <jason.m.lee.2003@gmail.com>
2025-11-29 21:58:51 -0500

    Added the ball and paddle for final variant

---
+++ variation-jam/js/tennispong.js
@@ -0,0 +1,17 @@
+// Third variant of pong game: Tennis Pong
+"use strict";
+
+// Tennis pong specific variables
+const tennisPongPaddle = {
+    x: 300,
+    y: 280,
+    width: 80,
+    height: 10
+};
+const tennisBall = {
+    x: 250,
+    y: 250,
+    size: 15,
+    speedX: 4,
+    speedY: -4
+};
