4db29f9fbb169700aa8266f3c5ece31314ed0d65
Jason Lee <jason.m.lee.2003@gmail.com>
2025-12-03 17:00:41 -0500

    Created global variables & breakPongsetup()

--- variation-jam/js/breakpong.js
+++ variation-jam/js/breakpong.js
@@ -15,3 +15,30 @@ const breakBall = {
     speedX: 4,
     speedY: -4
 }; 
+// Additional variables for breakout mechanics
+let breakoutBalls = [];
+let bricks = [];
+let breakoutPowerUps = [];
+let breakoutScore = 0;
+let breakoutLevel = 1;
+
+function breakPongSetup() {
+    // Reset paddle position
+    breakoutPaddle.x = width / 2 - breakoutPaddle.width / 2;
+    breakoutPaddle.y = 450;
+    breakoutPaddle.width = 100;
+    
+    breakoutBall.x = width / 2;
+    breakoutBall.y = 400;
+    breakoutBall.speedX = 3;
+    breakoutBall.speedY = -3;
+    breakoutBall.stuck = true;
+    
+    breakoutBalls = [];
+    breakoutPowerUps = [];
+    breakoutScore = 0;
+    breakoutLevel = 1;
+    gameOver = false;
+    
+    createBricks();
+}
